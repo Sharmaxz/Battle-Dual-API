e12be47e3fde6339db50c1748262cb9cd32efae2
Sharmaxz <shadowvonwolfkin@gmail.com>
2020-02-17 13:40:37 -0300

    Api with search

--- account/api/user.py
+++ account/api/user.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import serializers, viewsets, status
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from ..models import User
@@ -8,11 +11,17 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
+        extra_kwargs = {'password': {'write_only': True}}
+        exclude = ('username', 'is_superuser', 'is_admin', 'last_login',
+                   'is_staff', 'is_active', 'date_joined', 'groups', 'user_permissions')
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['nickname']
+    permission_classes = (IsAuthenticated,)
 
     def update(self, request, pk, *args, **kwargs):
         partial = kwargs.pop('partial', False)
