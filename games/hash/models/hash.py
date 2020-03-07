ac104085a59ef10d90d8dd4c8e54d97aac01b9a0
Sharmaxz <shadowvonwolfkin@gmail.com>
2020-03-07 18:48:24 -0300

    Created turn label

--- games/hash/models/hash.py
+++ games/hash/models/hash.py
@@ -1,10 +1,13 @@
-from django.db import models
 from matrix_field import MatrixField
+from django.db import models
+
+from account.models.user import User
 
 
 class Hash(models.Model):
     name = 'Hash'
     turn = models.ForeignKey('account.User', on_delete=models.CASCADE)
+    turn_label = models.CharField(max_length=255, null=True, blank=True)
     turn_count = models.PositiveIntegerField(default=0)
     matrix = MatrixField(datatype='int', dimensions=(3, 3), default=[[-1, -1, -1],[-1, -1, -1],[-1, -1, -1]])
     is_end = models.BooleanField()
@@ -12,3 +15,8 @@ class Hash(models.Model):
     class Meta:
         verbose_name = "Jogo da Velha"
         verbose_name_plural = "Jogos da Velha"
+
+    def save(self, *args, **kwargs):
+        self.turn_label = User.objects.get(pk=self.turn.id).nickname
+
+        super(Hash, self).save(*args, **kwargs)
